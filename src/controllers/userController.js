ca82c7227dd9fc4ae46e022120429f4e28c78c25
conal <develooper2022@gmail.com>
2023-05-30 13:47:16 +0700

    fix set cookie

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,13 +4,17 @@ const User = require("../models/User");
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
+    console.log(email, password);
     const account = await User.findOne({ email: email });
+    console.log(account);
     if (account) {
       const isAdmin = account.email === email && account.password === password;
+      console.log(isAdmin);
       if (isAdmin) {
         const token = jwt.sign({ id: account?._id }, process.env.SECRET_KEY, {
           expiresIn: "1d",
         });
+        console.log(token);
         res.cookie("token", token, { maxAge: 86400 });
         res.status(200).json({
           message: "Welcome to the admin dashboard",
