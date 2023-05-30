6f9e1c18f39be14b5de0eb6f70bc8ee014a1bc54
conal <develooper2022@gmail.com>
2023-05-30 14:04:55 +0700

    delete log

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,12 +4,9 @@ const User = require("../models/User");
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    console.log(email, password);
     const account = await User.findOne({ email: email });
-    console.log(account);
     if (account) {
       const isAdmin = account.email === email && account.password === password;
-      console.log(isAdmin);
       if (isAdmin) {
         const token = jwt.sign({ id: account?._id }, process.env.SECRET_KEY, {
           expiresIn: "1d",
